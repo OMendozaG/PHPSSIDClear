8038eeffdd654df1b7e708f3c1c3353a996075b1
OMendoza <OMendoza@SLNPE.com>
2022-02-03 19:34:20 +0100

    Log

--- PHPSSIDClear/Program.cs
+++ PHPSSIDClear/Program.cs
@@ -1,45 +1,81 @@
 ﻿using PHPSSIDClear;
 using System.IO;
 
-var Ini = new IniFile(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\Settings.ini");
+string ExePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\";
+var Ini = new IniFile(ExePath + "Settings.ini");
 
 int Deleted = 0;
+int DirError = 0;
+int FileError = 0;
+
 string Path = Ini.Read("Path");
 int Duration = Int32.Parse(Ini.Read("Duration"));
+bool Log = ((Ini.KeyExists("Log")) && (Ini.Read("Log") == "1")) ? true : false;
+
+if (File.Exists(ExePath + "Log.txt")) { File.Delete(ExePath + "Log.txt"); }
+
+Write("Starting PHPSIDClear on " + Path + " with " + Duration.ToString() + " duration");
+//Thread.CurrentThread.Priority = ThreadPriority.Lowest;
+ProcessDir(Path);
+Write("End! Deleted: " + Deleted.ToString() + " files, DirError: " + DirError + ", FileError: " + FileError);
+
+void Write(string text, bool write = false) {
+	Console.WriteLine(text);
+	if (!Log) { return; }
+
+	File.AppendAllText(ExePath + "Log.txt", "[" + DateTime.Now.ToString("HH:mm:ss") + "] " + text + Environment.NewLine);
+}
 
 void ProcessDir(string Folder) {
 
-	//Console.WriteLine("- Exploring " + Folder);
+	Write("- Exploring " + Folder);
 
-	DirectoryInfo Directory = new DirectoryInfo(Folder);
-	DirectoryInfo[] DirList = Directory.GetDirectories();
-	FileInfo[] FileList = Directory.GetFiles();
+	try {
+		DirectoryInfo Directory = new DirectoryInfo(Folder);
+		DirectoryInfo[] DirList = Directory.GetDirectories();
+		FileInfo[] FileList = Directory.GetFiles();
 
-	//Console.WriteLine("-- Directories: " + DirList.Length.ToString() + " / Files: " + FileList.Length.ToString());
+		Write("-- Directories: " + DirList.Length.ToString() + " / Files: " + FileList.Length.ToString());
 
-	if (FileList.Length > 0) {
-		DateTime now = DateTime.Now;
+		if (FileList.Length > 0) {
+			DateTime now = DateTime.Now;
+			int DeletedIn = 0;
 
-		foreach (FileInfo file in FileList) {
-			if ((now - file.LastWriteTime).TotalSeconds > Duration) {
-				//Console.WriteLine("--- Delete " + file.FullName + " " + file.LastWriteTime.ToString("dd/MM/yy HH:mm:ss") + " last edit " + (now - file.LastWriteTime).TotalSeconds + " seconds");
-				file.Delete();
-				Deleted++;
+			try {
+				foreach (FileInfo file in FileList) {
+					try {
+						if ((now - file.LastWriteTime).TotalSeconds > Duration) {
+							//Console.WriteLine("--- Delete " + file.FullName + " " + file.LastWriteTime.ToString("dd/MM/yy HH:mm:ss") + " last edit " + (now - file.LastWriteTime).TotalSeconds + " seconds");
+							try {
+								file.Delete();
+								Deleted++;
+								DeletedIn++;
+							} catch (Exception e) {
+								FileError++;
+								Write("File Error 1: " + e.ToString());
+							}
+						}
+					} catch (Exception e) {
+						FileError++;
+						Write("File Error 2: " + e.ToString());
+					}
+				}
+			} catch (Exception e) {
+				DirError++;
+				Write("Dir Error 1: " + e.ToString());
+			}
+
+			Write("-- Deleted: " + DeletedIn);
+		}
+
+		if (DirList.Length > 0) {
+			foreach (DirectoryInfo dir in DirList) {
+				ProcessDir(dir.FullName);
 			}
 		}
-	}
 
-	if (DirList.Length > 0) {
-		foreach (DirectoryInfo dir in DirList) {
-			ProcessDir(dir.FullName);
-		}
+	} catch (Exception e) {
+		DirError++;
+		Write("Dir Error 2: " + e.ToString());
 	}
 }
-
-Console.WriteLine("Starting PHPSIDClear on " + Path + " with " + Duration.ToString() + " duration");
-await Task.Run(() => {
-	Thread.CurrentThread.Priority = ThreadPriority.Lowest;
-	ProcessDir(Path);
-});
-
-Console.WriteLine("End! Deleted: " + Deleted.ToString() + " files");
